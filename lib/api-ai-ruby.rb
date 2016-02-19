da3adc8857d5453d9f618dbf519f7b99d21e2a9c
Matt Buck <techpeace@gmail.com>
2016-02-19 02:05:20 -0600

    Change the require for the RequestQuery -> Request rename

--- lib/api-ai-ruby.rb
+++ lib/api-ai-ruby.rb
@@ -2,7 +2,7 @@ require 'api-ai-ruby/constants'
 require 'api-ai-ruby/client'
 require 'api-ai-ruby/request_error'
 require 'api-ai-ruby/client_error'
-require 'api-ai-ruby/request_query'
+require 'api-ai-ruby/request'
 require 'api-ai-ruby/text_request'
 require 'api-ai-ruby/voice_request'
 
